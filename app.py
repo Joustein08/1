402ac92cf3616f104059353c5d4a38a1bc41db6d
Joustein08 <jodperezc@udistrital.edu.co>
2023-04-29 07:51:10 -0500

    1

---
+++ app.py
@@ -0,0 +1,5 @@
+# importar backend
+from backend.areas import *
+
+areaCirculo(100)
+areaCuadrado(100)
---
+++ backend/areas.py
@@ -0,0 +1,11 @@
+"""
+    Acá se van a calcular diferentes áreas
+"""
+
+import math
+
+def areaCirculo(radio):
+    print(math.pi*radio**2)
+
+def areaCuadrado(lado):
+    print(lado**2)
